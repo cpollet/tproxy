405a2cf2e42e678f09c919c82ca2153683280544
Christophe Pollet <cpollet@users.noreply.github.com>
2016-09-29 20:04:13 +0200

    supports multiple proxies

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ or
 $ mvn clean package
 $ cd tproxy
 $ mvn docker:build
-$ docker run -it --rm -p 8080:8080 -v `pwd`/../config/:/etc/tproxy cpollet/tproxy:VERSION
+$ docker run -it --rm -p 8080-8081:8080-8081 -v `pwd`/../config/:/etc/tproxy cpollet/tproxy:VERSION
 ```
 
-visit http://localhost:8080
+visit http://localhost:8080 or http://localhost:8081
--- tproxy/src/main/java/net/cpollet/tproxy/TProxy.java
+++ tproxy/src/main/java/net/cpollet/tproxy/TProxy.java
@@ -1,6 +1,7 @@
 package net.cpollet.tproxy;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -10,10 +11,14 @@ import io.netty.handler.logging.LoggingHandler;
 import io.netty.util.internal.logging.InternalLoggerFactory;
 import io.netty.util.internal.logging.Log4J2LoggerFactory;
 import net.cpollet.tproxy.configuration.Configuration;
+import net.cpollet.tproxy.configuration.ProxyConfiguration;
 import net.cpollet.tproxy.configuration.json.JsonConfiguration;
 import net.cpollet.tproxy.netty.ProxyFrontendHandler;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TProxy {
     private final static Logger LOG = LogManager.getLogger();
@@ -49,27 +54,36 @@ public class TProxy {
         EventLoopGroup bossGroup = new NioEventLoopGroup(1);
         EventLoopGroup workerGroup = new NioEventLoopGroup();
 
+        List<ChannelFuture> channelFutures = new ArrayList<>();
+
         try {
-            new ServerBootstrap()
-                    .group(bossGroup, workerGroup)
-                    .channel(NioServerSocketChannel.class)
-                    .handler(new LoggingHandler())
-                    .childHandler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        protected void initChannel(SocketChannel channel) throws Exception {
-                            channel.config().setAutoRead(false);
-                            channel.pipeline().addLast("log", new LoggingHandler());
-                            channel.pipeline().addLast("front", new ProxyFrontendHandler(
-                                    configuration.proxies().get(0).toHost(),
-                                    configuration.proxies().get(0).toPort(),
-                                    configuration.proxies().get(0).filterChain()));
-                        }
-                    })
-                    .bind(configuration.proxies().get(0).fromPort())
-                    .sync()
-                    .channel()
-                    .closeFuture()
-                    .sync();
+            for (final ProxyConfiguration proxyConfiguration : configuration.proxies()) {
+                channelFutures.add(
+                        new ServerBootstrap()
+                                .group(bossGroup, workerGroup)
+                                .channel(NioServerSocketChannel.class)
+                                .handler(new LoggingHandler())
+                                .childHandler(new ChannelInitializer<SocketChannel>() {
+                                    @Override
+                                    protected void initChannel(SocketChannel channel) throws Exception {
+                                        channel.config().setAutoRead(false);
+                                        channel.pipeline().addLast("log", new LoggingHandler());
+                                        channel.pipeline().addLast("front", new ProxyFrontendHandler(
+                                                proxyConfiguration.toHost(),
+                                                proxyConfiguration.toPort(),
+                                                proxyConfiguration.filterChain()));
+                                    }
+                                })
+                                .bind(proxyConfiguration.fromPort())
+                                .sync()
+                                .channel()
+                                .closeFuture()
+                );
+            }
+
+            for (ChannelFuture channelFuture : channelFutures) {
+                channelFuture.sync();
+            }
         }
         finally {
             bossGroup.shutdownGracefully();
